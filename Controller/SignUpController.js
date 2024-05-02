6890db3f66df21d1dfcf7e0b968d365f94f84d01
dinildamsith <dinildamsith52@gmail.com>
2024-05-02 13:15:26 +0530

    sign up done with ajax

---
+++ Controller/SignUpController.js
@@ -0,0 +1,31 @@
+import {SignUpModel} from "../Model/SignUpModel.js";
+
+// User SigUp
+$("#signUpBtn").on('click', () => {
+    var signUpEmail = $("#singUpEmailTxt").val();
+    var signUpPw    = $('#signUpPwTxt').val();
+    var name        = $('#name').val();
+    var selectRole  = $('#signUpSelectRoleOption').val();
+
+    var signupDetails = new SignUpModel(signUpEmail, signUpPw, name, selectRole);
+
+    var signUpDetailsJson = JSON.stringify(signupDetails);
+
+    const sendAJAX = (singUpDetails) => {
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:8080/shoes/api/v1/user/singUp",
+            contentType: "application/json",
+            data: singUpDetails,
+            success: function(data) {
+                alert("Success");
+            },
+            error: function(xhr, status, error) {
+                alert("Failed");
+            }
+        });
+    };
+    sendAJAX(signUpDetailsJson)
+
+
+});
---
+++ Model/SignUpModel.js
@@ -0,0 +1,8 @@
+export class SignUpModel {
+    constructor(signUpEmail, singUpPassword, name, signUpRole) {
+        this.email = signUpEmail;
+        this.password = singUpPassword;
+        this.name = name;
+        this.role = signUpRole;
+    }
+}
