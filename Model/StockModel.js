881dab880af47ef41cb5f40d4bd4a737b24cdf20
dinildamsith <dinildamsith52@gmail.com>
2024-05-03 22:02:48 +0530

    stock model and supplier model added

---
+++ Model/StockModel.js
@@ -0,0 +1,7 @@
+export class StockModel{
+    constructor(stockId,itemSize,qty) {
+        this.stockId = stockId;
+        this.itemSize = itemSize;
+        this.qty = qty
+    }
+}
---
+++ Model/SupplierModel.js
@@ -0,0 +1,14 @@
+export class SupplierModel{
+    constructor(id,name,category,address1,address2,address3,contact1,contact2,mail) {
+        this.supplierCode = id;
+        this.supplierName = name;
+        this.category = category;
+        this.addressLine1=address1;
+        this.addressLine2 = address2;
+        this.addressLine3 = address3;
+        this.contact1 = contact1;
+        this.contact2 = contact2;
+        this.email = mail;
+
+    }
+}
