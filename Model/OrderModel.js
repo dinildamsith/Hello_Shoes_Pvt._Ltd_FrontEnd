6ea80dc73dece184cfd0be85aa3849de001dafd2
dinildamsith <dinildamsith52@gmail.com>
2024-05-18 23:43:02 +0530

    order model added

---
+++ Model/OrderModel.js
@@ -0,0 +1,20 @@
+export class OrderModel{
+    constructor(orderCode,purchaseDate,customerName,itemDesc,size,unitPrice,qty,total,paymentMethod,points,cashierName,orderStatus,customerDetails,buyItem,employeeEntity,returnEntity) {
+        this.orderCode = orderCode;
+        this.purchaseDate = purchaseDate;
+        this.customerName = customerName;
+        this.itemDesc = itemDesc;
+        this.size = size;
+        this.unitPrice = unitPrice;
+        this.qty = qty;
+        this.total = total;
+        this.paymentMethod = paymentMethod;
+        this.points = points;
+        this.cashierName = cashierName;
+        this.orderStatus = orderStatus;
+        this.customerDetails = customerDetails;
+        this.buyItem = buyItem;
+        this.employeeEntity = employeeEntity;
+        this.returnEntity = returnEntity;
+    }
+}
